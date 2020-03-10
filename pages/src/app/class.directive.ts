e7bbb30d4013faefd16f53a256d7b09072363ff8
Miguel Ochoa <miguelalberto.ochoa@astrazeneca.com>
2020-03-10 15:37:53 -0600

    Intercepting property assignment for beter property control.

--- pages/src/app/class.directive.ts
+++ pages/src/app/class.directive.ts
@@ -4,13 +4,10 @@ import { Directive, ElementRef, Input } from '@angular/core';
   selector: '[appClass]'
 })
 export class ClassDirective {
-@Input() backgroundColor: string;
 
-  constructor(private element: ElementRef) {
+  constructor(private element: ElementRef) { }
 
-    // Never do this
-    setTimeout(() => {
-      this.element.nativeElement.style.backgroundColor = this.backgroundColor;
-    }, 50);
+  @Input() set backgroundColor(color: string) {
+    this.element.nativeElement.style.backgroundColor = color;
   }
 }
